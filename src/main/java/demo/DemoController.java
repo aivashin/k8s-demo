48919285ee5c4a15cd65bb2f3bf9acd7d782ba4e
Ubuntu <ubuntu@ip-172-31-45-157.eu-west-1.compute.internal>
2018-07-16 08:32:47 +0000

    nodeSelector added, replicas increased to 2, message changed

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!!!!!!!!!!!!!!!!!";
+        return "Greetings from Spring Boot! This is a new Image!!!";
     }
 
 }
