c403f9d3cb5cbe28c851b70141d7c7cd703495a1
Ubuntu <ubuntu@ip-172-31-45-157.eu-west-1.compute.internal>
2018-07-21 14:52:52 +0000

    Message for SQL exception changed

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -27,7 +27,7 @@ public class DemoController {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("SQLException: " + e.getMessage());
         }
  
         return conn;
