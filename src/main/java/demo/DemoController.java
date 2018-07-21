23e73f526ef8e270e79d3b56302eb5a0c54e3d67
Ubuntu <ubuntu@ip-172-31-45-157.eu-west-1.compute.internal>
2018-07-21 14:32:01 +0000

    Coonection to RDS PostgreSQL added

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -3,12 +3,33 @@ package demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @RestController
 public class DemoController {
 
+    private final String url = "jdbc:postgresql://k8s-demo.c7b9juowbyq0.eu-west-1.rds.amazonaws.com:5432/demo";
+    private final String user = "mapy";
+    private final String password = "MPaymentDB";
+
     @RequestMapping("/")
     public String index() {
+        Connection conn = connect();
         return "k8s-demo app is up and running!!!!!!!!!!!!!!!!!!!";
     }
 
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+ 
+        return conn;
+    }
 }
